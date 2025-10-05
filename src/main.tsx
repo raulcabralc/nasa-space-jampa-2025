a1d317754f505139da489bc2c532c66abef2efcd
Raul Cabral <raulcabralc@gmail.com>
2025-10-05 15:02:07 -0300

    feat(main): add routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,27 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import GlobalStyle from "./global-styles.styled.ts";
 import Header from "./layout/header/Header.tsx";
 import Content from "./layout/content/Content.styled.ts";
 import Index from "./pages/index/Index.tsx";
+import ChatBot from "./pages/chat-bot/ChatBot.tsx";
+import NotFound from "./pages/not-found/NotFound.tsx";
 
 const rootElement = document.getElementById("root");
 if (!rootElement) throw new Error("Root element not found");
 createRoot(rootElement).render(
   <StrictMode>
-    <GlobalStyle />
-    <Header />
-    <Content>
-      <Index />
-    </Content>
+    <BrowserRouter>
+      <GlobalStyle />
+      <Header />
+      <Content>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/ask" element={<ChatBot />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Content>
+    </BrowserRouter>
   </StrictMode>
 );
